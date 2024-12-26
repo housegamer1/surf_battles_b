616337c46c53eca2371056207651de8e10ca4a21
housegamer1 <housegamer@live.de>
2024-12-26 12:35:37 +0100

    logic and tests for teams and match

--- src/backend.py
+++ src/backend.py
@@ -2,9 +2,9 @@ import requests
 import json
 import time
 import datetime
-import datetime
 #import keyboard
 import os
+import collections
 
 
 ######################
@@ -36,17 +36,18 @@ class Team:
 # Match related stuff #
 #######################
 class Match:
-    id          = None
-    starttime   = None
-    duration    = None
-    teams       = []
-    surfmap     = None
-    zone        = None
-    leader      = None
+    id              = None
+    starttime       = None
+    duration        = None
+    teams           = []
+    surfmap         = None
+    zone            = None
+    leading_team    = None
+    leaderboard     = None
 
     def __init__(self, starttime, duration, surfmap, zone, teams) -> None:
         self.starttime = starttime
-        self.duration = duration
+        self.duration = duration #in minutes i guess. not sure if we even need this in the backend, since frontend likely tracks time
         self.surfmap = surfmap
         self.zone = zone
         self.teams = teams
@@ -54,9 +55,9 @@ class Match:
         idstring = ""
         for team in self.teams:
             for player in team.get_players():
-                idstring = idstring + player.get_id() + "_"
+                idstring = idstring + str(player.get_id()) + "_"
 
-        self.id = idstring + self.starttime
+        self.id = idstring + str(self.starttime)
 
     def get_id(self):
         return self.id
@@ -75,11 +76,73 @@ class Match:
     
     def get_zone(self):
         return self.zone
+    
+    def get_leading_team(self):
+        return self.leading_team
+    
+    def get_leaderboard(self):
+        return self.leaderboard
+    
+    def determine_leading_team(self):
+        team_times = [] #trying to make it so that in theory more than 2 teams could compete in a match
+
+        for team in self.teams:
+            team_sum = 0
+            players_set_times = 0
+
+            for player in team.get_players():
+                pb = player.get_personal_best(self.surfmap, self.zone)
+
+                if pb != None:
+                    team_sum = team_sum + pb
+                    players_set_times = players_set_times + 1
+
+            teamdict = {
+                "name": team.get_name(),
+                "times_set": players_set_times,
+                "sum_time": team_sum
+            }
+
+            team_times.append(teamdict.copy())
+
+        sorted_by_time = sorted(team_times, key=lambda item : (item["sum_time"]))
+        sorted_by_completions = sorted(sorted_by_time, key=lambda item : (item["times_set"]), reverse=True) # should be sorted by highest amount of completions with lowest time
+
+        self.leaderboard = sorted_by_completions
+        self.leading_team = self.leaderboard[0]
+
+
+
 
 
 ########################
 # Player related stuff #
 ########################
+class Record:
+    time        = None
+    timestamp   = None
+    map         = None
+    zone        = None
+
+    def __init__(self, time, timestamp, map , zone) -> None:
+        self.time = time
+        self.timestamp = timestamp
+        self.map = map
+        self.zone = zone
+
+    def get_time(self):
+        return self.time
+    
+    def get_timestamp(self):
+        return self.timestamp
+    
+    def get_map(self):
+        return self.map
+    
+    def get_zone(self):
+        return self.zone
+
+
 class Player:
     id      = None
     name    = None
@@ -87,26 +150,25 @@ class Player:
 
     def __init__(self, id) -> None:
         self.id = id
-        self.name = request_name(id)
+        self.name = request_name(self.id)
         self.records = []
 
     def add_time(self, settime, settimestamp, map, zone):
         global launchtime
-        finishstamp = datetime.datetime.strptime(settimestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
-        #print(self.get_name() + " Should add finish: " + map + " , " + str(settime) + "? " + str(finishstamp > launchtime) + " [ " + str(finishstamp) + " > " + str(launchtime) + " ]")
+        
+        if isinstance(settimestamp, datetime.datetime):
+            finishstamp = settimestamp
+        else:
+            finishstamp = datetime.datetime.strptime(settimestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+
         #prevent records from before program launch being recorded
         if finishstamp > launchtime:
 
-            finish = {
-                "time": settime,
-                "timestamp": settimestamp,
-                "map": map,
-                "zone": zone
-            }
+            finish = Record(settime, settimestamp, map, zone)
 
             for entry in self.records:
-                if entry["timestamp"] == settimestamp:
-                    return
+                if entry.get_timestamp() == settimestamp:
+                    return #time set at this timestamp already exists, skip.
 
             self.records.append(finish)
             #self.format_finish(settime, map, zone, True)
@@ -125,6 +187,17 @@ class Player:
 
     def get_records(self):
         return self.records
+    
+    def get_personal_best(self, surfmap, zone):
+        fastest_time = None
+        for record in self.records:
+            if record.get_map() == surfmap and record.get_zone() == zone:
+                time = record.get_time()
+
+                if fastest_time == None or time < fastest_time:
+                    fastest_time = time
+                
+        return fastest_time
 
     def format_finish(self, settime, map, zone, toprint):
         finishprint = self.get_name() + " finished "
@@ -238,7 +311,7 @@ def reload(arg):
 #############
 
 def request_name(id):
-    endpoint = shapi + "playerinfo/" + id
+    endpoint = shapi + "playerinfo/" + str(id)
     content = request(endpoint, 200)
 
     if content != None and len(content) == 1: #Endpoint returns [{}] for a single player
@@ -364,7 +437,7 @@ lastConfigReload = None
 lastResultCheck = None
 lastPollResult = None
 lastDrawnLeaderboard = None
-launchtime = datetime.datetime.utcnow()#.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+launchtime = datetime.datetime.now(datetime.timezone.utc)#.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 cfg = Config()
 
 ###########
---
+++ test/test_backend.py
@@ -0,0 +1,118 @@
+import src.backend
+import datetime
+
+#im no test engineer, go easy on me monkaS
+
+def test_record(): #wowee much useful
+    now = datetime.datetime.now(datetime.timezone.utc)
+    time = 60
+    map = "surf_blackheart"
+    zone = 0
+    rec = src.backend.Record(time, now, map, zone)
+
+    assert rec.get_time() == time
+    assert rec.get_timestamp() == now
+    assert rec.get_map() == map
+    assert rec.get_zone() == zone
+
+def test_add_time():
+    id = 37964988
+    now = datetime.datetime.now(datetime.timezone.utc)
+    time = 60
+    map = "surf_blackheart"
+    zone = 0
+
+    player = src.backend.Player(id)
+
+    assert player.get_id() == id
+    assert player.get_records() == []
+    assert player.get_personal_best(map, zone) == None
+
+    player.add_time(time, now, map, zone)
+    assert len(player.get_records()) == 1
+    assert player.get_personal_best(map, zone) == time
+    
+    
+def test_team():
+    player1_id = 921269561
+    player2_id = 287362850
+    player3_id = 106196572
+    player1 = src.backend.Player(player1_id)
+    player2 = src.backend.Player(player2_id)
+    player3 = src.backend.Player(player3_id)
+
+    team = src.backend.Team("Pakistan", [player1, player2])
+
+    assert len(team.get_players()) == 2
+    assert player1 in team.get_players() and player2 in team.get_players() and player3 not in team.get_players()
+
+    team.add_player(player3)
+
+    assert len(team.get_players()) == 3
+    assert player1 in team.get_players() and player2 in team.get_players() and player3 in team.get_players()
+
+    team.remove_player(player2)
+
+    assert len(team.get_players()) == 2
+    assert player1 in team.get_players() and player2 not in team.get_players() and player3 in team.get_players()
+
+
+def test_match():
+    player1_id = 38142345
+    player2_id = 37964988
+    player3_id = 246208267
+    player1 = src.backend.Player(player1_id)
+    player2 = src.backend.Player(player2_id)
+    player3 = src.backend.Player(player3_id)
+
+    team1 = src.backend.Team("Sweden", [player1])
+    team2 = src.backend.Team("Germany", [player2, player3])
+
+    starttime = datetime.datetime.now(datetime.timezone.utc)
+    duration = 10 #minutes
+    teams = [team1, team2]
+    surfmap = "surf_njv"
+    zone = 4
+
+    match = src.backend.Match(starttime, duration, surfmap, zone, teams)
+
+    assert match.get_id() == "38142345_37964988_246208267_" + str(starttime)
+    assert match.get_leading_team() == None
+    assert match.get_leaderboard() == None
+
+    match.determine_leading_team() # calling the function here just to see what happens when no pbs are set. any outcome is random as it sorts 2 entries with time 0 so im not gonna assert anything
+
+    newtime = datetime.datetime.now(datetime.timezone.utc) #program wont add times unless they are more recent than the match start
+    player1.add_time(50, newtime, surfmap, zone)
+    player2.add_time(60, newtime, surfmap, zone)
+    match.determine_leading_team()
+
+    team1_dict = {
+                "name": "Sweden",
+                "times_set": 1,
+                "sum_time": 50
+            }
+    team2_dict = {
+                "name": "Germany",
+                "times_set": 1,
+                "sum_time": 60
+            }
+
+    assert match.get_leading_team() == team1_dict
+    assert len(match.get_leaderboard()) == 2
+    assert match.get_leaderboard()[1] == team2_dict
+
+    player3.add_time(60, newtime, surfmap, zone)
+    match.determine_leading_team()
+    team2_dict = {
+                "name": "Germany",
+                "times_set": 2,
+                "sum_time": 120
+            }
+    
+    assert match.get_leading_team() == team2_dict
+    assert len(match.get_leaderboard()) == 2
+    assert match.get_leaderboard()[1] == team1_dict
+
+
+    
