e34265ebe69daee56e309b55650f5b54af1f897a
housegamer1 <housegamer@live.de>
2025-01-11 12:28:12 +0100

    match endpoint getters

--- src/main.py
+++ src/main.py
@@ -1,14 +1,39 @@
-from flask import Flask
+from flask import Flask, request
 import backend
 import threading
 import datetime
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
-    return backend.get_json(backend.matches[0].get_leaderboard())
+    return "Nothing to see here."
+
+@app.route("/matches")
+def matches():
+    return backend.get_json(backend.matches)
+
+@app.route("/match/<matchid>")
+def match(matchid):
+    for match in backend.matches:
+        if match.get_id() == matchid:
+            return backend.get_json(match)
+
+    return "Match ID not found"
+
+@app.route("/addmatch", methods=["POST"])
+def addmatch():
+    if request.content_type == "application/json":
+        data = request.json
+
+        if validate_request_data(data):
+            return "post success: " + str(data)
+        else:
+            return "post request missing essential data"
+
+    else:
+        return "Invalid content type. Please use application/json"
+
 
 @app.route("/settimes")
 def settimes():
@@ -23,6 +48,7 @@ def settimes():
     match.determine_leading_team()
     match.determine_team_delta()
     match.determine_player_delta()
+    return "Times set."
 
 @app.route("/newmatch")
 def newmatch():
@@ -44,6 +70,14 @@ def newmatch():
 
     backend.Match(starttime, duration, surfmap_match1, zone_match1, teams_match1)
     return "New match created!"
+
+def validate_request_data(data):
+    has_map = "map" in data
+    has_zone = "zone" in data
+    has_teams = "teams" in data
+    #TODO: improve this 
+
+    return has_map and has_zone and has_teams
 
 def main():
     #start main backend loop in a thread
